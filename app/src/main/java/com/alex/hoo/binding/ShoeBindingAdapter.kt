41fc0a24f158c6fa5bcdf029ee86f243227a936a
fengjw1 <fengjwxx@163.com>
2021-01-26 17:37:24 +0800

    20210126 second commit

--- app/src/main/java/com/alex/hoo/binding/ShoeBindingAdapter.kt
+++ app/src/main/java/com/alex/hoo/binding/ShoeBindingAdapter.kt
@@ -1,6 +1,5 @@
 package com.alex.hoo.binding
 
-import android.graphics.drawable.Drawable
 import android.util.Log
 import android.widget.EditText
 import android.widget.ImageView
@@ -15,26 +14,58 @@ import jp.wasabeef.glide.transformations.RoundedCornersTransformation
 fun bindImageFromUrl(view:ImageView,imageUrl:String?){
     if(!imageUrl.isNullOrEmpty()){
         Log.d("bindImageFromUrl", imageUrl)
-        Glide.with(view.context)
-            .load(imageUrl)
+        if (imageUrl.contains("a")){
+            loadCenterCrop(view, R.drawable.a)
+        }else if (imageUrl.contains("b")){
+            loadCenterCrop(view, R.drawable.b)
+        }else if (imageUrl.contains("c")){
+            loadCenterCrop(view, R.drawable.c)
+        }else if (imageUrl.contains("d")){
+            loadCenterCrop(view, R.drawable.d)
+        }else if (imageUrl.contains("e")){
+            loadCenterCrop(view, R.drawable.e)
+        }else if (imageUrl.contains("f")){
+            loadCenterCrop(view, R.drawable.f)
+        }
+
+    }
+}
+
+fun loadCenterCrop(view: ImageView, drawable: Int){
+    Glide.with(view.context)
+            .load(drawable)
 //            .placeholder(R.drawable.glide_placeholder)
             .centerCrop()
             .into(view)
-    }
 }
+
 
 // 加载带圆角的头像
 @BindingAdapter("imageTransFromUrl")
 fun bindImageTransFromUrl(view:ImageView,imageUrl:String?){
     if(!imageUrl.isNullOrEmpty()){
-        when(imageUrl){
-            is "a" ->
+        if (imageUrl.contains("a")){
+            loadRundCorner(view, R.drawable.a)
+        }else if (imageUrl.contains("b")){
+            loadRundCorner(view, R.drawable.b)
+        }else if (imageUrl.contains("c")){
+            loadRundCorner(view, R.drawable.c)
+        }else if (imageUrl.contains("d")){
+            loadRundCorner(view, R.drawable.d)
+        }else if (imageUrl.contains("e")){
+            loadRundCorner(view, R.drawable.e)
+        }else if (imageUrl.contains("f")){
+            loadRundCorner(view, R.drawable.f)
         }
-        Glide.with(view.context)
-            .load()
+
+    }
+}
+
+fun loadRundCorner(view: ImageView, drawable: Int){
+    Glide.with(view.context)
+            .load(drawable)
             .apply(bitmapTransform(RoundedCornersTransformation(20, 0, RoundedCornersTransformation.CornerType.ALL)))
             .into(view)
-    }
 }
 
 // 文本监听器
